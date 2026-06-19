125c935e3d582655d6c1db34b59dd6a7a8e14821
kapil soni <kapilsoni1883@gmail.com>
2026-06-19 13:52:57 +0530

    17/06/2026

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 function Stats() {
     return (
-        <div>
-            <h1>Stats</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Trust with confidence</h1>
+                    <h2> Customer-first always </h2>
+                    <p> That's why 1.3+ crore customers trust us with their investments </p>
+                </div>
+                <div className="col-6">
+                    <img 
+                        src="media/images/ecosystem.png"
+                        alt="Stats img"
+                        className="img-fluid"
+                        style={{width:"80%"}}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
