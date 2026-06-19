8db693e940d46d6b7e1cae8107b3adf2f87ce7c5
kapil soni <kapilsoni1883@gmail.com>
2026-06-19 13:55:11 +0530

    17/06/2026

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -1,28 +1,25 @@
-import React from "react";
+import React from 'react';
 
 function Stats() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">
-          <h1>Trust with confidence</h1>
-          <h2> Customer-first always </h2>
-          <p>
-            {" "}
-            That's why 1.3+ crore customers trust us with their investments{" "}
-          </p>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Trust with confidence</h1>
+                    <h2> Customer-first always </h2>
+                    <p> That's why 1.3+ crore customers trust us with their investments </p>
+                </div>
+                <div className="col-6">
+                    <img 
+                        src="media/images/ecosystem.png"
+                        alt="Stats img"
+                        className="img-fluid"
+                        style={{width:"80%"}}
+                    />
+                </div>
+            </div>
         </div>
-        <div className="col-6">
-          <img
-            src="media/images/ecosystem.png"
-            alt="Stats img"
-            className="img-fluid"
-            style={{ width: "80%" }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Stats;
